38134cccae3bd15d8e641df8882edabed85ed588
JaehongDev <println@kakao.com>
2022-10-10 12:27:47 +0900

    Feature(cars) :  랜덤한 값에 따라 이동 기능 추가 및 getter 메서드 추가

---
+++ src/main/java/racingcar/domain/car/Car.java
@@ -0,0 +1,29 @@
+package racingcar.domain.car;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    private final String name;
+    private       int    position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+    // 추가 기능 구현
+
+    // 앞으로 전진 혹은 정지
+    public void move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            this.position += 1;
+        }
+    }
+
+    // 자동차 이동 결과
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+}
